f63db90cf77366b8ea2461280776070112b3f6b7
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-01-30 01:37:09 +0300

    a

--- ETicaret-main/src/js/app.js
+++ ETicaret-main/src/js/app.js
@@ -1,29 +1,50 @@
-let sepet = [];
+let mysepet = [];
 let sepetGozuktu = false;
-$(document).ready(function () {
-    $(".sepetShow").hide();
-   for (let index = 0; index < Items.length; index++) {
-    
-    $("#mycards").append(' <div class="col"> <div class="card" id="'+Items[index].id+'" style="width: 18rem; height: 28rem;"></div> </div> ');
-    $("#"+Items[index].id).append('<img src="'+Items[index].imgurl+'" class="card-img-bottom" alt="..." style="width:95%; height:58%; margin-left: 3%; margin-top:2% ">');
-    $("#"+Items[index].id).append('<div class="card-body"><h5 class="card-title">'+Items[index].name +'</h5> <p class="card-text">'+Items[index].comment +'</p> <h5>'+Items[index].price +' TL</h5> <a  id="'+Items[index].id +'" class="btn btn-primary">Satın Al</a></div>');
-    
-   }
+let isSepetegit = false;
 
-   $("#mycards").children(".col").children(".card").children(".card-body").children("a").click(function (e) { 
-    sepet.push({"name":Items[$(this).attr("id")].name,"price":Items[$(this).attr("id")].price})
-    console.log(sepet)
-    $(".sepetShow").show();
-    if (sepetGozuktu == false){
-        $(".sepetShow").append('<h5 id="sepeth5"></h5> <a class="btn btn-danger" id="sepetegit" href="./src/pages/sepet/sepet.html">Sepete Git</a>');
-        $(".sepetShow").children("h5").text("Sepetinizde "+sepet.length +" ürün bulunmakta");
-        sepetGozuktu = true;
-    }else{
-        $(".sepetShow").children("h5").text("Sepetinizde "+sepet.length +" ürün bulunmakta");
+$(document).ready(function () {
+   function app(){
+        $(".sepetShow").hide();
+        for (let index = 0; index < Items.length; index++) {
         
+        $("#mycards").append(' <div class="col"> <div class="card" id="'+Items[index].id+'" style="width: 18rem; height: 28rem;"></div> </div> ');
+        $("#"+Items[index].id).append('<img src="'+Items[index].imgurl+'" class="card-img-bottom" alt="..." style="width:95%; height:58%; margin-left: 3%; margin-top:2% ">');
+        $("#"+Items[index].id).append('<div class="card-body"><h5 class="card-title">'+Items[index].name +'</h5> <p class="card-text">'+Items[index].comment +'</p> <h5>'+Items[index].price +' TL</h5> <a  id="'+Items[index].id +'" class="btn btn-primary" >Satın Al</a></div>');
+        
+        }
+    
+        $("#mycards").children(".col").children(".card").children(".card-body").children("a").click(function (e) { 
+        mysepet.push({"name":Items[$(this).attr("id")].name,"price":Items[$(this).attr("id")].price})
+        console.log(mysepet)
+        $(".sepetShow").show();
+        if (sepetGozuktu == false){
+            $(".sepetShow").append('<h5 id="sepeth5"></h5> <a class="btn btn-danger" id="sepetegit" >Sepete Git</a>');
+            $(".sepetShow").children("h5").text("Sepetinizde "+mysepet.length +" ürün bulunmakta");
+            sepetGozuktu = true;
+        }else{
+            $(".sepetShow").children("h5").text("Sepetinizde "+mysepet.length +" ürün bulunmakta");
+            
+        } 
+        $("#sepetegit").click(function (e) { 
+           $("body").html(" ");
+           for (let index = 0; index < mysepet.length; index++) {
+            $("body").append("<center><div><h3>"+mysepet[index].name+": "+mysepet[index].price +" TL</h3></div> </center>");
+            
+           }
+           $("body").append("<center><button id='mybtnOnay' class='btn btn-primary'>Onayla</button></center>");
+           $("#mybtnOnay").click(function (e) { 
+            alert("Rüyanıza Kargolandı")
+            
+           });
+            
+        });
+        
+        
+        
+        });
     }
+    app();
     
     
     
-   });
 }); 
--- ETicaret-main/src/js/urunler.js
+++ ETicaret-main/src/js/urunler.js
@@ -3,35 +3,42 @@ const Items = []
 Items[0] = {
     "id":0,
     "name":"Bilgisayar",
-    "price": 3000,
+    "price": 23000,
     "imgurl": "https://productimages.hepsiburada.net/s/285/300-443/110000273501775.jpg",
     "comment":"Bu Bir Oyun Bilgisayarı"
 }
 Items[1]= {
     "id":1,
     "name":"Konsol",
-    "price": 500,
+    "price": 15000,
     "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
     "comment": "Bu Bir Konsol"
 }
 Items[2]={
     "id":2,
-    "name":"Konsol",
-    "price": 500,
-    "imgurl": "https://lavinya.net/wp-content/uploads/2022/11/4c62ba-manzara-gol-lake-landscape-scaled.jpeg",
-    "comment": "Bu Bir Konsol"
+    "name":"Iphone",
+    "price": 35000,
+    "imgurl": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
+    "comment": "Bu Bir Iphone"
 }
 Items[3]= {
     "id":3,
-    "name":"Konsol",
-    "price": 500,
-    "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
+    "name":"Samsung Galaxy S21",
+    "price": 25000,
+    "imgurl": 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/131806-1-2_large.jpg',
     "comment": "Bu Bir Konsol"
 }
 Items[4]= {
     "id":4,
-    "name":"Konsol",
-    "price": 500,
-    "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
+    "name":"Xiaomi Mi 12 Pro",
+    "price": 19000,
+    "imgurl": "https://sc04.alicdn.com/kf/Ua3f8ea45f061445788dc77b593e3dd893.jpg",
     "comment": "Bu Bir Konsol"
+}
+Items[5]= {
+    "id":5,
+    "name":"Msi Gaming Laptop",
+    "price": 59000,
+    "imgurl": "https://www.incehesap.com/resim/urun/202201/61f65848ebe826.77378814_gklhpnjomeifq_500.jpg",
+    "comment": "Bu Bir Msi"
 }
