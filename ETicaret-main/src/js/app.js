fa130b85c947efc889c65d2547828364132fb8f7
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-01-29 23:15:53 +0300

    1.0

---
+++ ETicaret-main/src/js/app.js
@@ -0,0 +1,29 @@
+let sepet = [];
+let sepetGozuktu = false;
+$(document).ready(function () {
+    $(".sepetShow").hide();
+   for (let index = 0; index < Items.length; index++) {
+    
+    $("#mycards").append(' <div class="col"> <div class="card" id="'+Items[index].id+'" style="width: 18rem; height: 28rem;"></div> </div> ');
+    $("#"+Items[index].id).append('<img src="'+Items[index].imgurl+'" class="card-img-bottom" alt="..." style="width:95%; height:58%; margin-left: 3%; margin-top:2% ">');
+    $("#"+Items[index].id).append('<div class="card-body"><h5 class="card-title">'+Items[index].name +'</h5> <p class="card-text">'+Items[index].comment +'</p> <h5>'+Items[index].price +' TL</h5> <a  id="'+Items[index].id +'" class="btn btn-primary">Satın Al</a></div>');
+    
+   }
+
+   $("#mycards").children(".col").children(".card").children(".card-body").children("a").click(function (e) { 
+    sepet.push({"name":Items[$(this).attr("id")].name,"price":Items[$(this).attr("id")].price})
+    console.log(sepet)
+    $(".sepetShow").show();
+    if (sepetGozuktu == false){
+        $(".sepetShow").append('<h5 id="sepeth5"></h5> <a class="btn btn-danger" id="sepetegit" href="./src/pages/sepet/sepet.html">Sepete Git</a>');
+        $(".sepetShow").children("h5").text("Sepetinizde "+sepet.length +" ürün bulunmakta");
+        sepetGozuktu = true;
+    }else{
+        $(".sepetShow").children("h5").text("Sepetinizde "+sepet.length +" ürün bulunmakta");
+        
+    }
+    
+    
+    
+   });
+}); 
---
+++ ETicaret-main/src/js/urunler.js
@@ -0,0 +1,37 @@
+const Items = []
+
+Items[0] = {
+    "id":0,
+    "name":"Bilgisayar",
+    "price": 3000,
+    "imgurl": "https://productimages.hepsiburada.net/s/285/300-443/110000273501775.jpg",
+    "comment":"Bu Bir Oyun Bilgisayarı"
+}
+Items[1]= {
+    "id":1,
+    "name":"Konsol",
+    "price": 500,
+    "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
+    "comment": "Bu Bir Konsol"
+}
+Items[2]={
+    "id":2,
+    "name":"Konsol",
+    "price": 500,
+    "imgurl": "https://lavinya.net/wp-content/uploads/2022/11/4c62ba-manzara-gol-lake-landscape-scaled.jpeg",
+    "comment": "Bu Bir Konsol"
+}
+Items[3]= {
+    "id":3,
+    "name":"Konsol",
+    "price": 500,
+    "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
+    "comment": "Bu Bir Konsol"
+}
+Items[4]= {
+    "id":4,
+    "name":"Konsol",
+    "price": 500,
+    "imgurl": "https://cdn.akakce.com/z/sony/ps5-825-gb-ssd.jpg",
+    "comment": "Bu Bir Konsol"
+}
