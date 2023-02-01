14721f0e7de2306ced66dc4d1c61a7cc33a61f28
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-02-01 20:15:22 +0300

    urun update

--- eticaret-mainn/src/js/urunler.js
+++ eticaret-mainn/src/js/urunler.js
@@ -16,7 +16,7 @@ Items[1]= {
 }
 Items[2]={
     "id":2,
-    "name":"Iphone",
+    "name":"Iphone 13",
     "price": 35000,
     "imgurl": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204",
     "comment": "Bu Bir Iphone"
@@ -41,4 +41,25 @@ Items[5]= {
     "price": 59000,
     "imgurl": "https://www.incehesap.com/resim/urun/202201/61f65848ebe826.77378814_gklhpnjomeifq_500.jpg",
     "comment": "Bu Bir Msi"
+}
+Items[6]= {
+    "id":6,
+    "name":"çizim Tableti",
+    "price": 100,
+    "imgurl": "https://cdn.dsmcdn.com/ty381/product/media/images/20220403/16/81835944/434982128/1/1_org.jpg",
+    "comment": "Bu Bir Çizim Tableti"
+}
+Items[7]= {
+    "id":7,
+    "name":"Apple Watch Series 7",
+    "price": 8200,
+    "imgurl": "https://cdn.dsmcdn.com/ty209/product/media/images/20211025/15/155924729/268587438/1/1_org.jpg",
+    "comment": "Bu Bir Apple Watch"
+}
+Items[8]= {
+    "id":8,
+    "name":"Xiaomi Mi Band 5",
+    "price": 570,
+    "imgurl": "https://cdn.dsmcdn.com/ty6/product/media/images/20200713/10/4352101/76690477/1/1_org.jpg",
+    "comment": "Bu Bir Akıllı Bileklik"
 }
